0219d87a293974d08112cfefb2a9ed884ea025af
Gabriel Arno <agafarno@gmail.com>
2022-08-23 19:20:46 +0200

    commit

---
+++ week-3/InClass/exercise-B.js
@@ -0,0 +1,10 @@
+function boolChecker(bool) {
+    if (typeof bool === true) {
+        return "You've given me a bool, thanks!";
+    }
+
+    return "No bool, not cool.";
+}
+
+boolChecker(true);
+console.log(boolChecker);
