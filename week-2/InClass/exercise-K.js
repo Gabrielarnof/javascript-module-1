f0414cec8606e7f69f99ff7c928320f75d791d8b
Gabriel Arno <agafarno@gmail.com>
2022-08-24 14:34:56 +0200

    fisnih homewoek

---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,13 @@
+
+const students = ["Gabriel", "Natalia", "Jacobo", "Andrea", "Alejandro"];
+function showName(array) {
+
+    for (let i = 0; i < students.length; i++) {
+        const studentsMessage = "& name of the student is: " + students[i];
+        const indexMessage = "Index number is: " + i;
+        console.log(indexMessage, studentsMessage);
+
+    }
+}
+
+showName(students);
