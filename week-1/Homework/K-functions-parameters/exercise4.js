6e459cc1b3a59e36a64c9061ef8cfb10cee24c64
Gabriel Arno <agafarno@gmail.com>
2022-08-24 11:17:58 +0200

    exercie  4,5,6

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
-// Declare your function first
 
-// Call the function and assign to a variable `sum`
+function sum(a, b) {
+    return a + b;
+}
 
-console.log(sum);
+var result = sum(13, 124);
+console.log(result);
--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,21 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+function toUpper(phrase) {
+    var upperCased = phrase.toUpperCase();
+    return upperCased;
+}
+function greeting(name, greetingStart) {
+    var greeting = `${greetingStart} ${name}`;
+
+    return toUpper(greeting);
+}
+var greetingStart = "hello";
+
+console.log(greeting(mentor1, greetingStart));
+console.log(greeting(mentor2, greetingStart));
+console.log(greeting(mentor3, greetingStart));
+console.log(greeting(mentor4, greetingStart));
+console.log(greeting(mentor5, greetingStart));
