fa11e903568601ba35c8a0f626afd645fc8b8bfe
Gabriel Arno <agafarno@gmail.com>
2022-08-30 20:08:47 +0200

    A-B in  class

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,7 @@
+const nameOfPeople = ["Gabriel  ", "Daniel", "Kike", "Gabriela"];
+const numberOfPeople = nameOfPeople.length;
+console.log(nameOfPeople);
+console.log(numberOfPeople);
+nameOfPeople[0] = "Amy";
+nameOfPeople[4] = "Carlos";
+console.log(nameOfPeople);
--- week-3/InClass/exercise-B.js
+++ week-3/InClass/exercise-B.js
@@ -1,10 +1,15 @@
-function boolChecker(bool) {
-    if (typeof bool === true) {
-        return "You've given me a bool, thanks!";
+const Studentinclass = ["Gabriel", "Natalia", "Andrea", "Winston", "Xiomara", "Alejandro", "Matias", "Simon", "Jacobo", "Winston", "Karelys"];
+const TotalFriends = Studentinclass.concat("Hanna");
+console.log(TotalFriends);
+const alphabeticalOrder = TotalFriends.sort();
+console.log(alphabeticalOrder);
+
+function nameFcn(name, array) {
+    if (array.includes(name)) {
+        return `${name} is at the class with ${array}`;
+    } else {
+        return `${name} is not at the class with  ${array}`;
     }
-
-    return "No bool, not cool.";
 }
-
-boolChecker(true);
-console.log(boolChecker);
+console.log(nameFcn("Gabriel", TotalFriends));
+console.log(nameFcn("Pedro", TotalFriends));
