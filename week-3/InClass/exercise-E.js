0db97e3e0b834e649d288cefb3120afa04c9b61a
Gabriel Arno <agafarno@gmail.com>
2022-09-03 12:17:22 +0200

    Update exercise-E.js

--- week-3/InClass/exercise-E.js
+++ week-3/InClass/exercise-E.js
@@ -2,14 +2,13 @@ const years = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
 
 function birthYear(years) {
     years.forEach((year) => {
-        let ages = 2022 - year;
+        let ages = new Date().getFullYear() - year;
         console.log(ages);
-
     });
 
 };
 birthYear(years);
-const getAge = (birthYear) => 2022 - birthYear;
+const getAge = (birthYear) => new Date().getFullYear() - birthYear;
 const birthYearOfPeople = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
 const peoplesAges = birthYearOfPeople.map(getAge);
 console.log(peoplesAges);
